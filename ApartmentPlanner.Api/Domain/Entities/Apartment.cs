21ca71f2667f0d122ffadc2b1573a20eab770733
Erick Castilho <erick.jansons@hotmail.com>
2026-02-14 17:28:12 -0300

    Created entities and enum

---
+++ ApartmentPlanner.Api/Domain/Entities/Apartment.cs
@@ -0,0 +1,11 @@
+﻿namespace ApartmentPlanner.Api.Domain.Entities
+{
+    public class Apartment
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+        public int CreatedByUserId { get; private set; }
+        public DateTime DeliveredAt { get; private set; }
+    }
+}
---
+++ ApartmentPlanner.Api/Domain/Entities/ApartmentMember.cs
@@ -0,0 +1,13 @@
+﻿using ApartmentPlanner.Api.Domain.Enums;
+
+namespace ApartmentPlanner.Api.Domain.Entities
+{
+    public class ApartmentMember
+    {
+        public int Id { get; private set; }
+        public int ApartmentId { get; private set; }
+        public int UserId { get; private set; }
+        public MemberRole Role { get; private set; }
+        public DateTime JoinedAt { get; private set; }
+    }
+}
