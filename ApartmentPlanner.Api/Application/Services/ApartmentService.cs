c9cd74e7387ae208afadfa4ebea5c0c6eba59711
Erick Castilho <erick.jansons@hotmail.com>
2026-02-14 22:57:53 -0300

    Criado método async para criação de Apartment

--- ApartmentPlanner.Api/Application/Services/ApartmentService.cs
+++ ApartmentPlanner.Api/Application/Services/ApartmentService.cs
@@ -1,5 +1,6 @@
 ﻿using ApartmentPlanner.Api.Domain.Entities;
 using ApartmentPlanner.Api.Infrastructure.Data;
+using ApartmentPlanner.Api.Domain.Enums;
 
 namespace ApartmentPlanner.Api.Application.Services;
 
@@ -10,5 +11,20 @@ public class ApartmentService
     public ApartmentService (AppDbContext context)
     {
         _context = context;
+    }
+
+    public async Task<int> CreateApartmentAsync(string name, int userId, DateTime? deliveredAt)
+    {
+        var apartment = new Apartment(name, userId, deliveredAt);
+
+        _context.Apartments.Add(apartment);
+        await _context.SaveChangesAsync(); // <- gerar o ID do apartamento salvando primeiro (para não ficar zerado)
+
+        var member = new ApartmentMember(apartment.Id, userId, MemberRole.Owner);
+
+        _context.ApartmentMembers.Add(member);
+        await _context.SaveChangesAsync();
+
+        return apartment.Id;
     }
 }
