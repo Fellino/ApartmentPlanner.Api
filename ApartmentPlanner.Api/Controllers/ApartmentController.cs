52c198b2348b0ebf8d75e326c4e6ea4210839d94
Erick Castilho <erick.jansons@hotmail.com>
2026-02-14 23:15:27 -0300

    Criado controller e DTO para Apartment

---
+++ ApartmentPlanner.Api/Controllers/ApartmentController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using ApartmentPlanner.Api.Application.Services;
+using ApartmentPlanner.Api.Application.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApartmentPlanner.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ApartmentController : ControllerBase
+{
+    private readonly ApartmentService _apartmentService;
+
+    public ApartmentController(ApartmentService apartmentService)
+    {
+        _apartmentService = apartmentService;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(CreateApartmentRequest request)
+    {
+        var id = await _apartmentService.CreateApartmentAsync(request.Name, request.UserId, request.DeliveredAt);
+
+        return CreatedAtAction(nameof(Create), new { id }, new { id });
+    }
+}
