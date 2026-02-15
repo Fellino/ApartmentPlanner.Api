eab080cbd68b2bbe0f75da4ed18386aaf9f718fa
Erick Castilho <erick.jansons@hotmail.com>
2026-02-14 21:39:21 -0300

    Criado pasta Services e ApartmentService

---
+++ ApartmentPlanner.Api/Application/Services/ApartmentService.cs
@@ -0,0 +1,14 @@
+﻿using ApartmentPlanner.Api.Domain.Entities;
+using ApartmentPlanner.Api.Infrastructure.Data;
+
+namespace ApartmentPlanner.Api.Application.Services;
+
+public class ApartmentService
+{
+    private readonly AppDbContext _context;
+
+    public ApartmentService (AppDbContext context)
+    {
+        _context = context;
+    }
+}
--- ApartmentPlanner.Api/Program.cs
+++ ApartmentPlanner.Api/Program.cs
@@ -1,3 +1,4 @@
+using ApartmentPlanner.Api.Application.Services;
 using ApartmentPlanner.Api.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 
@@ -10,7 +11,7 @@ builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
+builder.Services.AddScoped<ApartmentService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
