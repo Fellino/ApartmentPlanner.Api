adde7850391e70bee310807bdfac665af97ce5f4
Erick Castilho <erick.jansons@hotmail.com>
2026-02-15 16:37:53 -0300

    Update Contribution Service

--- ApartmentPlanner.Api/Application/Services/ContributionService.cs
+++ ApartmentPlanner.Api/Application/Services/ContributionService.cs
@@ -3,6 +3,7 @@ using ApartmentPlanner.Api.Domain.Entities;
 using ApartmentPlanner.Api.Domain.Enums;
 using ApartmentPlanner.Api.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
 
 namespace ApartmentPlanner.Api.Application.Services;
 
@@ -32,12 +33,7 @@ public class ContributionService
             throw new Exception("Tipo de contribuição inválida.");
         if (type == ContributionType.Withdrawal)
         {
-            var totalDeposits = await _context.Contributions.Where(c => c.ApartmentId == request.ApartmentId && c.Type == ContributionType.Deposit)
-                .SumAsync(c => c.Amount);
-            var totalWhitdrawals = await _context.Contributions.Where(c => c.ApartmentId == request.ApartmentId && c.Type == ContributionType.Withdrawal)
-                .SumAsync(c => c.Amount);
-            var balance = totalDeposits - totalWhitdrawals;
-
+            var balance = await CalculateBalanceAsync(request.ApartmentId);
             if (request.Amount > balance)
                 throw new Exception("Saldo insuficiente para realizar a retirada.");
         }
@@ -46,5 +42,22 @@ public class ContributionService
 
         _context.Contributions.Add(contribution);
         await _context.SaveChangesAsync();
+    }
+    private async Task<decimal> CalculateBalanceAsync(int apartmentId)
+    {
+        var totalDeposits = await _context.Contributions.Where(c => c.ApartmentId == apartmentId && c.Type == ContributionType.Deposit)
+            .SumAsync(c => c.Amount);
+        var totalWithdrawals = await _context.Contributions.Where(c => c.ApartmentId == apartmentId && c.Type == ContributionType.Withdrawal)
+            .SumAsync(c => c.Amount);
+        return totalDeposits - totalWithdrawals;
+    }
+    public async Task<decimal> GetBalanceAsync(int apartmentId)
+    {
+        var apartment = await _context.Apartments.FirstOrDefaultAsync(ap => ap.Id == apartmentId);
+        if (apartment == null)
+        {
+            throw new Exception("Apartamento não foi encontrado.");
+        }
+        return await CalculateBalanceAsync(apartmentId);
     }
 }
