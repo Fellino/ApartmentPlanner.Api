e349f5c2e724185c69cca48efa4f588abc7e9582
Erick Castilho <erick.jansons@hotmail.com>
2026-02-15 17:16:56 -0300

    Criado DTOs para autenticação, AuthService e AuthController, além de adicionar geração de hash de senha

---
+++ ApartmentPlanner.Api/Application/Services/AuthService.cs
@@ -0,0 +1,37 @@
+﻿using ApartmentPlanner.Api.Domain.Entities;
+using ApartmentPlanner.Api.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using ApartmentPlanner.Api.Application.DTOs;
+
+
+namespace ApartmentPlanner.Api.Application.Services;
+
+public class AuthService
+{
+    private readonly AppDbContext _context;
+    private readonly PasswordHasher<User> _passwordHasher;
+
+    public AuthService(AppDbContext context)
+    {
+        _context = context;
+        _passwordHasher = new PasswordHasher<User>();
+    }
+
+    public async Task RegisterAsync(RegisterRequest request)
+    {
+        // Verificar se o email já está cadastrado
+        var userExists = await _context.Users.AnyAsync(u => u.Email == request.Email);
+        if (userExists)
+            throw new Exception("Email já cadastrado");
+        // Criar um usuário temporário para gerar o hash da senha
+        var tempUser = new User(request.Name, request.Email, "temp");
+        // Gerar o hash da senha usando o PasswordHasher
+        var hash = _passwordHasher.HashPassword(tempUser, request.Password);
+        var user = new User(request.Name, request.Email, hash);
+
+        _context.Users.Add(user);
+
+        await _context.SaveChangesAsync();
+    }
+}
